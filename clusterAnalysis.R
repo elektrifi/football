7b85a24f66fbceb2a084c0523135f415017e6c21
elektrifi <elektrifi@gmail.com>
2015-12-29 17:36:04 +0000

    Added data labels if >= 90 points

--- clusterAnalysis.R
+++ clusterAnalysis.R
@@ -10,19 +10,21 @@ player_full_df<-player_full_df[-1,]
 player_full_df$full_name<-paste(player_full_df$first_name, player_full_df$second_name, sep = " ")
 #
 player_name_v<-c(player_full_df$full_name)
+player_last_name_v<-c(as.character(player_full_df$second_name))
 player_type_v<-c(as.character(player_full_df$type_name))
 player_club_v<-c(as.character(player_full_df$team_name))
 player_total_points_v<-c(as.numeric(player_full_df$total_points))
 
 # Initialise & get basic stats
-player_row_df <- data.frame(player_name_v, player_type_v, player_club_v, player_total_points_v, stringsAsFactors = TRUE)
+player_row_df <- data.frame(player_name_v, player_type_v, player_club_v, player_total_points_v, player_last_name_v, stringsAsFactors = TRUE)
 # Dedupe the data.frame, just in case!
 player_row_df<-unique(player_row_df)
 
 # See the current relationship between type_name and points grouped into club
 library(ggplot2)
+library(plyr)
 #
-plot1 <- ggplot(player_row_df, aes(x=player_row_df$player_club_v, y=player_row_df$player_total_points_v, color=player_row_df$player_type_v)) + geom_point(size=3)
+plot1 <- ggplot(player_row_df, aes(x=player_club_v, y=player_total_points_v, color=player_type_v) ) + geom_point(size=4)
 # Add a title
 plot1 <- plot1 + ggtitle("Total Points Per Player by Club")
 plot1 <- plot1 + theme(plot.title = element_text(size=16, face="bold", vjust=2))
@@ -32,15 +34,17 @@ plot1 <- plot1 + labs(x="Club", y="Total Points Per Player")
 plot1 <- plot1 + theme(axis.title.x = element_text(face="bold", colour="#000000", size=16), axis.text.x  = element_text(angle=90, vjust=0.5, size=16))
 plot1 <- plot1 + theme(axis.title.y = element_text(face="bold", colour="#000000", size=16), axis.text.y  = element_text(size=16))
 # Make legend title more appealing
-plot1 <- plot1 + scale_color_manual("Position\n", c("Defender", "Forward", "Goalkeeper", "Midfielder" ), values=c("red", "green", "purple", "blue" ))
+plot1 <- plot1 + scale_color_manual("Position\n", c("Defender", "Forward", "Goalkeeper", "Midfielder" ), values=c("red", "green", "purple", "blue"))
 #plot1 <- plot1 + scale_shape_discrete(name="Position")
 # plot1 <- plot1 + scale_fill_discrete(name="Player\nPosition",
 #                          breaks=c("Defender", "Forward", "Goalkeeper", "Midfielder" ),
 #                          labels=c("Defender", "Forward", "Goalkeeper", "Midfielder" ) )
 # Change legend appearance
-plot1 <- plot1 + theme(legend.title = element_text(colour="black", size=144, face="bold"))
+plot1 <- plot1 + theme(legend.title = element_text(colour="black", size=14, face="bold"))
 # Change legend label Label appearance
 plot1 <- plot1 + theme(legend.text = element_text(colour="black", size = 14))
+# Add data labels (but only if over a certain value of points)
+plot1 <- plot1 + geom_text(data=subset(player_row_df, player_total_points_v >= 90), aes(y=player_total_points_v+5, label=(player_last_name_v)  ) )
 #
 plot1 #draw plot1
 #
@@ -55,3 +59,5 @@ playerPointsCluster
 # Plot the cluster
 playerPointsCluster$cluster <- as.factor(playerPointsCluster$cluster)
 ggplot(player_row_df, aes(player_row_df$player_club_v, player_row_df$player_total_points_v, color= playerPointsCluster$cluster)) + geom_point()
+#
+ggplot(player_row_df, aes(player_row_df$player_total_points_v, player_row_df$player_total_points_v, color= playerPointsCluster$cluster)) + geom_point()
